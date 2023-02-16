da78db059a1c7578327040e8d5b55357c1b544ba
Swhag <khg5293@outlook.com>
2023-02-16 01:15:42 -0500

    post to db added in server.js

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const MongoClient = require('mongodb').MongoClient;
 
+var db;
 MongoClient.connect(
   'mongodb+srv://admin:password1234@cluster0.iv5k38x.mongodb.net/?retryWrites=true&w=majority',
   function (error, client) {
     if (error) return console.log(error);
+
+    db = client.db('todo-app');
+
     app.listen(8080, function () {
       console.log('listening on 8080');
     });
@@ -33,6 +37,14 @@ app.get('/pages', function (req, res) {
 });
 
 app.post('/add', function (req, res) {
-  res.send('submit complete');
-  console.log(req.body);
+  res.send('Data submitted');
+  console.log(req.body.taskName);
+  console.log(req.body.taskDue);
+
+  db.collection('post').insertOne(
+    { taskName: req.body.taskName, taskDue: req.body.taskDue },
+    function () {
+      console.log('data saved');
+    }
+  );
 });
